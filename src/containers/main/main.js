da907595d1a6ae88acc2f6f4ef7a23cbaa7e1449
Noah Hall <noah.hall@dictionary.com>
2016-10-02 09:03:31 -0700

    update validation for text area

--- src/containers/main/main.js
+++ src/containers/main/main.js
@@ -35,8 +35,7 @@ class Main extends Component {
     if (el.willValidate && !el.validity.valid) {
       el.className = 'has-error';
       el.previousSibling.firstElementChild.innerHTML = el.validationMessage;
-    }else {
-      el.setCustomValidity('');
+    } else {
       el.className = '';
       el.previousSibling.firstElementChild.innerHTML = el.validationMessage;
     }
@@ -48,113 +47,98 @@ class Main extends Component {
         <h2>
           Create an event
         </h2>
-        <div>
-          <section>
-            <label htmlFor='eventname'>Event Name<span className='error' />
-            </label>
-            <input
-              id='eventname'
-              maxLength={20}
-              minLength={2}
-              onBlur={this.checkValidOnBlur}
-              pattern='\w+'
-              placeholder='Name of your event'
-              required
-              title='Give your event a memorable name between two and twenty characters'
-              type='text'
-            />
-          </section>
-        </div>
-        <div>
-          <section>
-            <label htmlFor='eventhostname'>Event Host Name<span className='error' />
-            </label>
-            <input
-              id='eventhostname'
-              maxLength={20}
-              minLength={2}
-              onBlur={this.checkValidOnBlur}
-              pattern='\w+'
-              placeholder='Person or Organization hosting the event'
-              required
-              title='Enter between two and twenty characters'
-              type='text'
-            />
-          </section>
-        </div>
-        <div>
-          <section>
-            <label htmlFor='eventstart'>Starts<span className='error' />
-            </label>
-            <input
-              id='eventstart'
-              onBlur={this.checkValidOnBlur}
-              placeholder='Date and time the event starts'
-              required
-              type='date'
-            />
-          </section>
-        </div>
-        <div>
-          <section>
-            <label htmlFor='eventend'>Ends<span className='error' />
-            </label>
-            <input
-              id='eventend'
-              onBlur={this.checkValidOnBlur}
-              placeholder='Date and time the event ends'
-              required
-              type='date'
-            />
-          </section>
-        </div>
-        <div>
-
-          <section>
-            <label htmlFor='location'>Location<span className='error' />
-            </label>
-            <textarea
-              id='location'
-              onBlur={this.checkValidOnBlur}
-              placeholder='Location (and directions) to event' />
-          </section>
-        </div>
-        <div>
-
-          <section>
-            <label htmlFor='eventtype'>Event Type<span className='error' />
-            </label>
-            <input
-              id='eventtype'
-              list='eventtypelist'
-              placeholder='Type of event'
-              type='text'
-            />
-            <datalist id='eventtypelist'>
-              <select>
-                <option value='birthday'>Birthday Party</option>
-                <option value='conference'>Conference</option>
-                <option value='wedding'>Wedding</option>
-                <option value='other' >Other</option>
-              </select>
-            </datalist>
-          </section>
-        </div>
-        <div>
-
-          <section>
-            <label htmlFor='guestlist'>Guest list
-            </label>
-            <textarea id='guestlist' placeholder='Enter one name per line' />
-          </section>
-        </div>
-        <div>
-          <section>
-            <label htmlFor='message'>Message<span className='error' />
-            </label>
-            <textarea id='message' placeholder='Optional message to the guests with additional information about the event' />
-          </section>
-        </div>
+        <section>
+          <label htmlFor='eventname'>Event Name<span className='error' />
+          </label>
+          <input
+            id='eventname'
+            maxLength={20}
+            minLength={2}
+            onBlur={this.checkValidOnBlur}
+            pattern='\w+'
+            placeholder='Name of your event'
+            required
+            title='Give your event a memorable name between two and twenty characters'
+            type='text'
+          />
+        </section>
+        <section>
+          <label htmlFor='eventhostname'>Event Host Name<span className='error' />
+          </label>
+          <input
+            id='eventhostname'
+            maxLength={20}
+            minLength={2}
+            onBlur={this.checkValidOnBlur}
+            pattern='\w+'
+            placeholder='Person or Organization hosting the event'
+            required
+            title='Enter between two and twenty characters'
+            type='text'
+          />
+        </section>
+        <section>
+          <label htmlFor='eventstart'>Starts<span className='error' />
+          </label>
+          <input
+            id='eventstart'
+            onBlur={this.checkValidOnBlur}
+            placeholder='Date and time the event starts'
+            required
+            type='date'
+          />
+        </section>
+        <section>
+          <label htmlFor='eventend'>Ends<span className='error' />
+          </label>
+          <input
+            id='eventend'
+            onBlur={this.checkValidOnBlur}
+            placeholder='Date and time the event ends'
+            required
+            type='date'
+          />
+        </section>
+        <section>
+          <label htmlFor='location'>Location<span className='error' />
+          </label>
+          <textarea
+            id='location'
+            maxLength={20}
+            minLength={2}
+            onBlur={this.checkValidOnBlur}
+            placeholder='Location (and directions) to event'
+            required
+          />
+        </section>
+        <section>
+          <label htmlFor='eventtype'>Event Type<span className='error' />
+          </label>
+          <input
+            id='eventtype'
+            list='eventtypelist'
+            placeholder='Type of event'
+            type='text'
+          />
+          <datalist id='eventtypelist'>
+            <select>
+              <option value='birthday'>Birthday Party</option>
+              <option value='conference'>Conference</option>
+              <option value='wedding'>Wedding</option>
+              <option value='other' >Other</option>
+            </select>
+          </datalist>
+        </section>
+        <section>
+          <label htmlFor='guestlist'>Guest list
+          </label>
+          <textarea id='guestlist' placeholder='Enter one name per line' />
+        </section>
+        <section>
+          <label htmlFor='message'>Message<span className='error' />
+          </label>
+          <textarea id='message' placeholder='Optional message to the guests with additional information about the event' />
+        </section>
         <section>
           <input type='submit' />
         </section>
